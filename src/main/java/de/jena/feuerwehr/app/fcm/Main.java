483c481e568c3060c4d5981c6a91db42883456cc
Konstanius <earlygamerhd@gmail.com>
2024-04-21 14:37:37 +0200

    Removes debug information for Production use

--- src/main/java/de/jena/feuerwehr/app/fcm/Main.java
+++ src/main/java/de/jena/feuerwehr/app/fcm/Main.java
@@ -1,4 +1,4 @@
-package de.hbvjena90;
+package de.jena.feuerwehr.app.fcm;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -16,9 +16,13 @@ public class Main {
 
     static final FileInputStream serviceAccount;
 
-    static final FirebaseOptions options;
+    static FirebaseOptions options;
 
     static final AndroidConfig androidConfigHigh = AndroidConfig.builder()
+            .setNotification(AndroidNotification.builder().build())
+            .setPriority(AndroidConfig.Priority.HIGH).build();
+
+    static final AndroidConfig androidConfigLow = AndroidConfig.builder()
             .setPriority(AndroidConfig.Priority.NORMAL).build();
 
     static FirebaseApp app;
@@ -31,7 +35,7 @@ public class Main {
         }
     }
 
-    static {
+    public static void main(String[] args) {
         try {
             options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -39,9 +43,7 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
 
-    public static void main(String[] args) {
         boolean debugMode = args.length > 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -58,14 +60,13 @@ public class Main {
                 } else {
                     jsonObject = new JSONObject();
                     jsonObject.put("method", "tokens");
-                    jsonObject.put("type", "user_verified");
+                    jsonObject.put("type", "test_silent");
 
                     System.out.println("Enter the token to test to:");
                     String token = scanner.nextLine();
 
-                    System.out.println("Is this token an iOS token? (y/n)");
-                    String ios = scanner.nextLine();
-                    boolean isIos = ios.equalsIgnoreCase("y") || ios.equalsIgnoreCase("yes");
+                    System.out.println("Please input the type: (\"genericLow\", \"android\", \"ios\")");
+                    String type = scanner.nextLine();
 
                     JSONObject data = new JSONObject();
                     long millis = System.currentTimeMillis();
@@ -74,16 +75,27 @@ public class Main {
                     data.put("id", String.valueOf(randomId));
                     jsonObject.put("data", data);
                     JSONObject tokens = new JSONObject();
-                    if (isIos) {
-                        tokens.put("ios", new JSONArray().put(token));
+
+
+                    if (type.equalsIgnoreCase("genericLow")) {
+                        tokens.put("genericLow", new JSONArray().put(token));
                         tokens.put("android", new JSONArray());
-                    } else {
+                        tokens.put("ios", new JSONArray());
+                    } else if (type.equalsIgnoreCase("android")) {
+                        tokens.put("genericLow", new JSONArray());
                         tokens.put("android", new JSONArray().put(token));
                         tokens.put("ios", new JSONArray());
+                    } else if (type.equalsIgnoreCase("ios")) {
+                        tokens.put("genericLow", new JSONArray());
+                        tokens.put("android", new JSONArray());
+                        tokens.put("ios", new JSONArray().put(token));
+                    } else {
+                        System.err.println("Invalid type!");
+                        continue;
                     }
                     jsonObject.put("tokens", tokens);
 
-                    System.out.println("Sending test message to " + (isIos ? "iOS" : "Android") + " with ID " + randomId + "...");
+                    System.out.println("Sending test_silent message to " + token + " with type " + type + " and id " + randomId + "...");
                 }
 
                 String method = jsonObject.getString("method");
@@ -107,15 +119,22 @@ public class Main {
                         //   }
                         // }
                         JSONObject tokens = jsonObject.getJSONObject("tokens");
+
                         JSONArray androidTokensArray = tokens.getJSONArray("android");
                         JSONArray iosTokensArray = tokens.getJSONArray("ios");
+                        JSONArray genericLowTokensArray = tokens.getJSONArray("genericLow");
+
                         HashSet<String> androidTokens = new HashSet<>();
                         HashSet<String> iosTokens = new HashSet<>();
+                        HashSet<String> genericLowTokens = new HashSet<>();
                         for (int i = 0; i < androidTokensArray.length(); i++) {
                             androidTokens.add(androidTokensArray.getString(i));
                         }
                         for (int i = 0; i < iosTokensArray.length(); i++) {
                             iosTokens.add(iosTokensArray.getString(i));
+                        }
+                        for (int i = 0; i < genericLowTokensArray.length(); i++) {
+                            genericLowTokens.add(genericLowTokensArray.getString(i));
                         }
 
                         String type = jsonObject.getString("type");
@@ -160,17 +179,56 @@ public class Main {
                             System.out.println("Sent " + (tokenSubset.size() - response.getFailureCount()) + " ANDROID message(s)" + (tokenSubset.size() - response.getFailureCount() == 1 ? "" : "s") + " to " + tokenSubset.size() + " token" + (tokenSubset.size() == 1 ? "" : "s"));
                         }
 
+                        ApnsConfig iosConfigLow = ApnsConfig.builder()
+                                .setAps(Aps.builder().setContentAvailable(true).build())
+                                .putHeader("apns-priority", "5")
+                                .build();
+
+                        // treat the Android tokens with silent push notifications
+                        while (!genericLowTokens.isEmpty()) {
+                            // Get the first 500 tokens
+                            HashSet<String> tokenSubset = new HashSet<>();
+                            Iterator<String> iterator = genericLowTokens.iterator();
+                            for (int i = 0; i < 500 && iterator.hasNext(); i++) {
+                                tokenSubset.add(iterator.next());
+                            }
+
+                            // Remove the first 500 tokens from the original set
+                            genericLowTokens.removeAll(tokenSubset);
+
+                            // Prepare the message
+                            MulticastMessage message = MulticastMessage.builder()
+                                    .putData("type", type)
+                                    .putAllData(dataMap)
+                                    .addAllTokens(tokenSubset)
+                                    .setAndroidConfig(androidConfigLow)
+                                    .setApnsConfig(iosConfigLow)
+                                    .build();
+
+                            // Get the response
+                            BatchResponse response = FirebaseMessaging.getInstance(app).sendMulticast(message);
+
+                            if (response.getFailureCount() > 0) {
+                                List<SendResponse> responses = response.getResponses();
+                                for (int i = 0; i < responses.size(); i++) {
+                                    if (!responses.get(i).isSuccessful()) {
+                                        System.err.println("MALTOKEN " + tokenSubset.toArray()[i]);
+                                    }
+                                }
+                            }
+                            System.out.println("Sent " + (tokenSubset.size() - response.getFailureCount()) + " GENERIC LOW message(s)" + (tokenSubset.size() - response.getFailureCount() == 1 ? "" : "s") + " to " + tokenSubset.size() + " token" + (tokenSubset.size() == 1 ? "" : "s"));
+                        }
+
                         ApnsConfig iosConfigHigh = ApnsConfig.builder()
                                 .setAps(
                                         Aps.builder()
                                                 .setContentAvailable(false)
                                                 .setMutableContent(true)
-                                                .setSound("default")
+                                                .setSound(CriticalSound.builder().setName("res_alarm_1.mp3").setVolume(1).build())
                                                 .build()
                                 )
                                 .putHeader("apns-priority", "10")
                                 .putHeader("apns-push-type", "alert")
-                                .putHeader("apns-expiration", "0")
                                 .build();
 
                         // treat the iOS tokens with alerting push notifications
@@ -185,16 +243,6 @@ public class Main {
                             // Remove the first 500 tokens from the original set
                             iosTokens.removeAll(tokenSubset);
 
-                            // add the intent to test the data
-                            JSONObject payload = new JSONObject();
-                            payload.put("type", "openProfile");
-                            payload.put("userId", "1");
-                            JSONObject contentModelMap = new JSONObject();
-                            contentModelMap.put("payload", payload);
-                            JSONObject modelMap = new JSONObject();
-                            modelMap.put("content", contentModelMap);
-                            dataMap.put("data", modelMap.toString());
-
                             // Prepare the message
                             MulticastMessage message = MulticastMessage.builder()
                                     .putData("type", type)
@@ -203,8 +251,8 @@ public class Main {
                                     .setApnsConfig(iosConfigHigh)
                                     .setNotification(
                                             Notification.builder()
-                                                    .setTitle("(V-S) Synchronisierung fehlgeschlagen")
-                                                    .setBody("Du hast eine Benachrichtigung erhalten, aber die Synchronisierung ist fehlgeschlagen. Öffne die App, um die Synchronisierung zu starten.")
+                                                    .setTitle("Alarmierung (Synchronisierungsfehler)")
+                                                    .setBody("Eine Alarmierung wurde gesendet. Bitte überprüfe die App auf neue Alarmierungen.")
                                                     .build()
                                     )
                                     .build();
@@ -221,28 +269,6 @@ public class Main {
                                 }
                             }
                             System.out.println("Sent " + (tokenSubset.size() - response.getFailureCount()) + " IOS message(s)" + (tokenSubset.size() - response.getFailureCount() == 1 ? "" : "s") + " to " + tokenSubset.size() + " token" + (tokenSubset.size() == 1 ? "" : "s"));
-                        }
-                        break;
-                    case "topic":
-                        String topic = jsonObject.getString("topic");
-                        data = jsonObject.getJSONObject("data");
-                        data.put("type", jsonObject.getString("type"));
-
-                        dataMap = new HashMap<>();
-                        for (String key : data.keySet()) {
-                            dataMap.put(key, data.getString(key));
-                        }
-
-                        Message message = Message.builder()
-                                .setAndroidConfig(androidConfigHigh)
-                                .putAllData(dataMap)
-                                .setTopic(topic)
-                                .build();
-
-                        try {
-                            FirebaseMessaging.getInstance(app).send(message);
-                        } catch (FirebaseMessagingException e) {
-                            System.err.println("MALTOPIC " + topic);
                         }
                         break;
                 }
